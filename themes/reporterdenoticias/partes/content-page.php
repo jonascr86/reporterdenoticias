e7c96a0750baaf03d5fbbad41958c7dad17fcff4
n <jonascr86@gmail.com>
2017-03-27 23:42:18 -0300

    correção do conteúdo das páginas

--- themes/reporterdenoticias/partes/content-page.php
+++ themes/reporterdenoticias/partes/content-page.php
@@ -24,76 +24,19 @@
         <div class="row">
             <div class="col-sm-12">
                 <div class="history-info">
-                    <p class="title">História, Missão, Vissão</p>
+                    <p class="title"><?php bloginfo('name'); ?></p>
                     <h2 class="subtitle">
-                        Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore.
+                        <?php bloginfo('description'); ?>
                     </h2>
                     <div class="row">
-                        <div class="col-sm-6">
-                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. </p>
+                        <div class="col-sm-10">
+                            <p><?php the_content(); ?> </p>
                         </div>
-                        <div class="col-sm-6">
-                            <p>
-                                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-                            </p>
-                        </div>
+                        
                     </div> <!-- //row -->
                 </div> <!-- //advertisment-info -->
             </div>
         </div> <!-- //row -->
-
-        <div class="our-history">
-            <div class="row">
-                <div class="col-sm-8">
-                    <p class="title">Monthly Uniques Vistors</p>
-                    <p>
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit.
-                    </p>            
-                </div>
-                <div class="col-sm-3 col-sm-offset-1">
-                    <div class="counter">
-                        <h3 class="title">65M</h3>
-                        <p>
-                            Monthly Uniques Vistors
-                        </p>
-                    </div> <!-- //counter -->
-                </div>
-            </div> <!-- //row -->
-
-            <div class="row">
-                <div class="col-sm-8">
-                    <p class="title">Social Media Followers</p>
-                    <p>
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit.
-                    </p>            
-                </div>
-                <div class="col-sm-3 col-sm-offset-1">
-                    <div class="counter">
-                        <h3 class="title">100K</h3>
-                        <p>
-                            Monthly Uniques Vistors
-                        </p>
-                    </div> <!-- //counter -->
-                </div>
-            </div> <!-- //row -->
-
-            <div class="row">
-                <div class="col-sm-8">
-                    <p class="title">Monthly Shares</p>
-                    <p>
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit.
-                    </p>            
-                </div>
-                <div class="col-sm-3 col-sm-offset-1">
-                    <div class="counter">
-                        <p class="title">21K</p>
-                        <p>
-                            Monthly Uniques Vistors
-                        </p>
-                    </div> <!-- //counter -->
-                </div>
-            </div> <!-- //row -->
-        </div> <!-- //our-history -->
     </div> <!-- //container -->
 </section>
 <!--====  End of MAIN WRAPPER  ====-->
